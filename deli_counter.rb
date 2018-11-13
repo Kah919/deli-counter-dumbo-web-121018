ff2dc830069d5bb8066a52b655c7721bc5866aff
Kah Yap <kahyap13@gmail.com>
2018-11-13 00:25:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@
 katz_deli = []
 
 def line
-  if katz_deli.length == 0
-    return "The line is currently empty."
-  end
+
+end
+
+def take_a_number(katz_deli, name)
+  return "Welcome, #{name}. You are number #{katz_deli.push(name).length} in line."
 end
