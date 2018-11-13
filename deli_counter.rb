19948b19dc911fca6d982301cf1d6bfb772a6dd6
Kah Yap <kahyap13@gmail.com>
2018-11-13 01:47:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,7 @@ end
 def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.push(name).length} in line."
 end
+
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli.shift}"
+end
