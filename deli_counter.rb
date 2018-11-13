32747e8da9ea3bc7d6d956e6495cebb2b03e31ec
Kah Yap <kahyap13@gmail.com>
2018-11-13 01:40:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   msg = "The line is currently: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, idx|
-      msg += "#{idx + 1}. #{name}"
+      msg += "#{idx + 1}. #{name} "
     end
     puts msg
   else
